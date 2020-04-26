b41d6b9a0acb5096cf47026f34834e2520e387dd
Yannick <20306025+yandeu@users.noreply.github.com>
2020-04-26 14:33:59 +0200

    Bundle the three-wrapper into two CJS modules

---
+++ packages/threeWrapper/webpack.index.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  }
+}
