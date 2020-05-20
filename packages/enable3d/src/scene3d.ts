16a8c5e0bafce69a269291be265cad23b65806d7
Yannick <20306025+yandeu@users.noreply.github.com>
2020-05-20 16:47:39 +0200

    Fix auto update animationMixers

--- packages/enable3d/src/scene3d.ts
+++ packages/enable3d/src/scene3d.ts
@@ -200,6 +200,8 @@ export class Scene3D implements Partial<ThreeGraphics> {
     this.update?.(parseFloat(time.toFixed(3)), parseInt(delta.toString()))
     this.physics.update(delta)
     this.physics.updateDebugger()
+
+    this.animationMixers.update(delta)
     this.renderer.render(this.scene, this.camera)
   }
 }
