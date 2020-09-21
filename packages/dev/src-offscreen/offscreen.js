dd7e10b247cd1ffd3c58d55d2cacaba5340517aa
HexaField <joshfield999@gmail.com>
2020-09-21 14:39:13 +1000

    Revert "adds OffscreenCanvas dev environment"
    This reverts commit 698325d0a8318b3b5f96981557ec13d086af8bd1.

--- packages/dev/src-offscreen/offscreen.js
+++
@@ -1,7 +0,0 @@
-import init from './scene.js';
-
-self.onmessage = function ( message ) {
-	if(!message.data) return
-	var data = message.data;
-	init( data.canvas, { innerWidth: data.width, innerHeight: data.height} , data.pixelRatio );
-};
