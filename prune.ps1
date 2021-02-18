7c2c073edb41a4d21d5f7f907950bf47cf3b168b
yandeu <20306025+yandeu@users.noreply.github.com>
2021-02-18 12:49:43 +0100

    Added simple prune script

---
+++ prune.ps1
@@ -0,0 +1,10 @@
+Write-Output "Removing node_modules ..."
+Get-ChildItem -Include node_modules -Recurse -force | Remove-Item -Force -Recurse
+
+Write-Output "Removing dist ..."
+Get-ChildItem -Include dist -Recurse -force | Remove-Item -Force -Recurse
+
+Write-Output "Removing package-lock.json ..."
+Get-ChildItem -Include package-lock.json -Recurse -force | Remove-Item -Force -Recurse
+
+Write-Output "DONE!"
