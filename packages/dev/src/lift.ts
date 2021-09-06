fb5548c6b0c782b0d71eb54db4e48d1de1b46d27
yandeu <20306025+yandeu@users.noreply.github.com>
2021-09-07 00:43:27 +0200

    dev

--- packages/dev/src/lift.ts
+++ packages/dev/src/lift.ts
@@ -1,10 +1,5 @@
 import { ExtendedObject3D, PhysicsLoader, Project, Scene3D, THREE } from 'enable3d'
 
-import { TypeBufferGeometry, processGeometry } from '@enable3d/ammo-physics/dist/tmp'
-import { Material, Object3D, Quaternion, SkinnedMesh, Vector3 } from 'three'
-import { Vector } from 'matter'
-import { timeStamp } from 'console'
-
 class MainScene extends Scene3D {
   leftArm: ExtendedObject3D
   rightArm: ExtendedObject3D
@@ -12,43 +7,48 @@ class MainScene extends Scene3D {
   async create() {
     this.physics.debug?.enable()
 
-    this.warpSpeed()
+    const { orbitControls } = await this.warpSpeed()
+    orbitControls?.target.set(0, 2, 0)
+    this.camera.lookAt(0, 2, 0)
 
-    this.physics.add.box({ x: 0, y: 1, name: 'box' })
+    const box = this.make.box({ x: 0.2, y: 5, name: 'box' })
+    box.rotateY(1)
+    box.rotateX(1)
+    this.add.existing(box)
+    this.physics.add.existing(box)
 
-    this.leftArm = this.physics.add.box({ mass: 100, x: -1.2, y: 3, height: 5, width: 0.2, collisionFlags: 2 })
+    this.leftArm = this.physics.add.box({ mass: 100, x: -2, y: 1, height: 2, width: 0.2, depth: 2, collisionFlags: 2 })
     this.leftArm.body.setFriction(1)
     this.leftArm.userData.move = true
 
-    this.rightArm = this.physics.add.box({ mass: 100, x: 1.2, y: 3, height: 5, width: 0.2, collisionFlags: 2 })
+    this.rightArm = this.physics.add.box({ mass: 100, x: 2, y: 1, height: 2, width: 0.2, depth: 2, collisionFlags: 2 })
     this.rightArm.body.setFriction(1)
     this.rightArm.userData.move = true
 
-    this.leftArm.body.on.collision(otherObject => {
-      if (otherObject.name === 'box') this.leftArm.userData.move = false
+    this.leftArm.body.on.collision((otherObject, event) => {
+      if (otherObject.name === 'box' && event === 'start') this.leftArm.userData.move = false
+      else if (otherObject.name === 'box' && event === 'end') this.leftArm.userData.move = true
     })
 
-    this.rightArm.body.on.collision(otherObject => {
-      if (otherObject.name === 'box') this.rightArm.userData.move = false
+    this.rightArm.body.on.collision((otherObject, event) => {
+      if (otherObject.name === 'box' && event === 'start') this.rightArm.userData.move = false
+      else if (otherObject.name === 'box' && event === 'end') this.rightArm.userData.move = true
     })
   }
 
   update(time: number) {
-    if (time > 1 && time < 2.5) {
-      if (this.rightArm.userData.move) this.rightArm.position.x -= 0.01
-      if (this.leftArm.userData.move) this.leftArm.position.x += 0.01
+    if (time < 2) return
 
-      this.rightArm.body.needUpdate = true
-      this.leftArm.body.needUpdate = true
-    }
+    if (this.leftArm.userData.move) this.leftArm.position.x += 0.01
+    if (this.rightArm.userData.move) this.rightArm.position.x -= 0.01
 
-    if (time > 3) {
+    if (!this.leftArm.userData.move && !this.rightArm.userData.move) {
       this.rightArm.position.y += 0.01
       this.leftArm.position.y += 0.01
-
-      this.rightArm.body.needUpdate = true
-      this.leftArm.body.needUpdate = true
     }
+
+    this.rightArm.body.needUpdate = true
+    this.leftArm.body.needUpdate = true
   }
 }
 
