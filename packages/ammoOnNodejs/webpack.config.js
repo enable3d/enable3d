675cdc79be3f13b1f3e5f88c1b57578c6757b347
yandeu <20306025+yandeu@users.noreply.github.com>
2021-12-05 17:12:54 +0100

    Update webpack.config.js

--- packages/ammoOnNodejs/webpack.config.js
+++ packages/ammoOnNodejs/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
+const optimization = require('../../webpack/optimization.cjs')
 
 module.exports = {
   mode: 'production',
+  stats: 'errors-warnings',
   entry: './src/index.ts',
   target: 'node',
   // devtool: 'source-map',
@@ -11,16 +13,10 @@ module.exports = {
     libraryTarget: 'commonjs'
   },
   resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
     extensions: ['.ts', '.tsx', '.js']
   },
   module: {
-    rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' }
-    ]
+    rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }]
   },
-  optimization: {
-    minimize: true
-  }
+  ...optimization
 }
