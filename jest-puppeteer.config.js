76538db810433d2047aae83d1490b8cbad9cdee2
yandeu <20306025+yandeu@users.noreply.github.com>
2021-12-05 17:04:10 +0100

    Update jest-puppeteer.config.js

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   launch: {
     headless: true,
-    args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox'],
-    ignoreDefaultArgs: ['--disable-extensions']
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    // args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox'],
+    // ignoreDefaultArgs: ['--disable-extensions']
     // dumpio: true
     // slowMo: 250
   }
