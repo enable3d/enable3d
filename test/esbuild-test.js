fb9ec72201312d7add33527c6aaa3e0721dde8f6
yandeu <20306025+yandeu@users.noreply.github.com>
2022-05-23 21:27:23 +0200

    Test esbuild bundler

---
+++ test/esbuild-test.js
@@ -0,0 +1,6 @@
+/**
+ * Test if if builds successfully with esbuild
+ * See: https://github.com/enable3d/enable3d/issues/287
+ */
+
+import { Project, Scene3D, PhysicsLoader } from '../packages/enable3d'
