1efc4eb0d2ff098a1a99e025c874be1969f4406c
yandeu <20306025+yandeu@users.noreply.github.com>
2023-06-24 22:14:50 +0200

    dev

--- packages/dev/src/loop.ts
+++ packages/dev/src/loop.ts
@@ -1,3 +1,13 @@
 import type { Robot, Sensors } from './robot'
 
-export const loop = (sensors: Sensors, robot: Robot) => {}
+export const loop = (sensors: Sensors, robot: Robot) => {
+  const { rli } = sensors
+
+  robot.motor.forward()
+
+  if (rli < 0.8) {
+    robot.motor.left()
+  } else {
+    robot.motor.right()
+  }
+}
