9895b80a54b88a09e754f708974297cc4ffbcf67
yandeu <20306025+yandeu@users.noreply.github.com>
2024-12-20 18:45:34 +0100

    dev

--- packages/enable3d/src/scene3d.ts
+++ packages/enable3d/src/scene3d.ts
@@ -9,9 +9,8 @@ import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer
 import { Clock, WebGLRenderer } from 'three'
 import { ExtendedMesh, ExtendedObject3D } from '@enable3d/common/dist/types.js'
 import { AmmoPhysics } from '@enable3d/ammo-physics'
-import { CSG } from '@enable3d/three-graphics/dist/csg'
-
-import * as Plugins from '@enable3d/three-graphics/dist/plugins'
+import { CSG } from '@enable3d/three-graphics/dist/csg/index.js'
+import * as Plugins from '@enable3d/three-graphics/dist/plugins/index.js'
 
 // https://www.typescriptlang.org/docs/handbook/utility-types.html Pick<T,K>
 // export class Scene3D implements Partial<ThreeGraphics> {
